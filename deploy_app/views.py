f085f900b2971c9b287741419c229a8534b01083
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2025-03-22 08:46:32 +0530

    Resolved home.html

--- deploy_app/views.py
+++ deploy_app/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'deploy_app/ home.html')
+    return render(request, 'deploy_app/home.html')
