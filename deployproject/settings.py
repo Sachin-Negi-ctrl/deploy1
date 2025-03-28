4794be2e2ccab664fe1d1764de1f5b7a1496e5cd
Sachin-Negi-ctrl <sachinnegizxzx@gmail.com>
2025-03-28 21:42:30 +0530

    test02

--- deployproject/settings.py
+++ deployproject/settings.py
@@ -132,3 +132,7 @@ STATICSTORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# for testing
